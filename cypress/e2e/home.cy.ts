a851c2b76ccae3744f72722b307a57530ce5fe42
Lyudmyla Kalmykova <omnipotence8.45@gmail.com>
2022-10-13 01:30:31 +0300

    How to test forms

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -14,4 +14,23 @@ describe("home page", () => {
         cy.get("dt").eq(1).contains("25+ Lessons")
         cy.get("dt").eq(2).contains("Free and Open Source")
     })
+
+    it("allows users to subscribe to the email list", () => {
+        cy.getByData("email-input").type("tom@aol.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains("tom@aol.com")
+    })
+
+    it("does NOT allow an invalid email address", () => {
+        cy.getByData("email-input").type("tom")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+ 
+    it("users cannot sign up for our newsletter if they are already subscribed", () => {
+        cy.getByData("email-input").type("john@example.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message").should("exist").contains("Error: john@example.com already exists.")
+    })
+
 })
